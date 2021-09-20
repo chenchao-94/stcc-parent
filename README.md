f9c94551bb652f22d85bd7dac7ced9ba6ef66f68
chenchao-94 <772799040@qq.com>
2021-09-20 18:07:40 +0800

    添加网关

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # stcc-parent
-## 1、分布式框架
+~~~
+com.stcc     
+├── stcc-gateway         // 网关模块 [8080]
+├── stcc-authorization   // 认证中心 [9200]
+├── stcc-api             // 接口模块
+│       └── ruoyi-api-system                         // 系统接口
+├── stcc-common          // 通用模块
+│       └── stcc-common-core                         // 核心模块
+│       └── stcc-common-datascope                    // 权限范围
+│       └── stcc-common-datasource                   // 多数据源
+│       └── stcc-common-log                          // 日志记录
+│       └── stcc-common-redis                        // 缓存服务
+│       └── stcc-common-security                     // 安全模块
+│       └── stcc-common-swagger                      // 系统接口
+├── stcc-business         // 业务模块
+├── stcc-visual          // 图形化管理模块
+│       └── stcc-visual-monitor                      // 监控中心 [9100]
+├──pom.xml                // 公共依赖
+~~~
 
-## 集成
-#### 1、XXL-JOB分布式任务调度平台
---
+++ stcc-gateway/src/main/java/com/stcc/gateway/StccGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.stcc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 包名 com.stcc.gateway
+ * 说明 网关启动
+ * 作者 stcc
+ * 创建时间 2021/9/14
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class StccGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StccGatewayApplication.class, args);
+        System.out.println("  ===============超叔网关启动成功=============== \n " +
+                "  ________  ___________  ______    ______   \n" +
+                " /\"       )(\"     _   \")/\" _  \"\\  /\" _  \"\\  \n" +
+                "(:   \\___/  )__/  \\\\__/(: ( \\___)(: ( \\___) \n" +
+                " \\___  \\       \\\\_ /    \\/ \\      \\/ \\      \n" +
+                "  __/  \\\\      |.  |    //  \\ _   //  \\ _   \n" +
+                " /\" \\   :)     \\:  |   (:   _) \\ (:   _) \\  \n" +
+                "(_______/       \\__|    \\_______) \\_______) \n");
+    }
+}
